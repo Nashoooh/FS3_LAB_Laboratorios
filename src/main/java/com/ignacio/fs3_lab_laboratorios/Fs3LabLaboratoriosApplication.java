9e2749598878c77fc009567f2b8dffbef5d20dd8
IgnacioAndana <56287207+Nashoooh@users.noreply.github.com>
2025-10-28 22:08:58 -0300

    [RELEASE] Primer commit

---
+++ src/main/java/com/ignacio/fs3_lab_laboratorios/Fs3LabLaboratoriosApplication.java
@@ -0,0 +1,13 @@
+package com.ignacio.fs3_lab_laboratorios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fs3LabLaboratoriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fs3LabLaboratoriosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ignacio/fs3_lab_laboratorios/Fs3LabLaboratoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignacio.fs3_lab_laboratorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fs3LabLaboratoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
